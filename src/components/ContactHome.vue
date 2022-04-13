6e96487463273455c9cfb36e03575a03b209bd11
ramon_puon <ramon.puon@gmail.com>
2022-04-13 15:37:09 -0500

    contact me

--- src/components/ContactHome.vue
+++ src/components/ContactHome.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        
-    </div>
+    <section>
+        <h2>Contacto</h2>
+        <p>Nos da mucho gusto tu interés en nosotros. Déjanos saber como podemos ayudarte</p>
+        <div class="contact_container">
+            <form action="" class="contact_form">
+                <div class="contact_content">
+                    <label for="" class="contact_label">Nombre</label>
+                    <input type="text" class="contact_input">
+                </div>
+
+                <div class="contact_content">
+                    <label for="" class="contact_label">Correo electrónico</label>
+                    <input type="text" class="contact_input">
+                </div>
+
+                <div class="contact_content">
+                    <label for="" class="contact_label">¿Como podemos ayudarle</label>
+                    <textarea name="message" id="" cols="0" rows="10" class="contact_input"></textarea>
+                </div>
+            </form>
+        </div>
+    </section>
 </template>
 
 <script>
