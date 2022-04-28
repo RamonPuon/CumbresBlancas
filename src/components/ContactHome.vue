e4037fd42530f7b9e9d1867bdd3177283afae458
ramon_puon <ramon.puon@gmail.com>
2022-04-28 11:06:29 -0500

    Contact format

--- src/components/ContactHome.vue
+++ src/components/ContactHome.vue
@@ -1,8 +1,8 @@
 <template>
-    <section>
-        <h2>Contacto</h2>
+    <section class="contact_container">
+        <h2 class="contact_title">Contacto</h2>
         <p>Nos da mucho gusto tu interés en nosotros. Déjanos saber como podemos ayudarte</p>
-        <div class="contact_container">
+        <div class="form_container">
             <form action="" class="contact_form">
                 <div class="contact_content">
                     <label for="" class="contact_label">Nombre</label>
@@ -30,5 +30,37 @@ export default {
 </script>
 
 <style>
+   
+    .contact_container {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding-left: 10rem;
+    }
+    .contact_title{
+        font-size: 3rem;
+        align-content: left;
+        margin-bottom: 0;
+        color: #2c3e50;
+    }
+
+    .form_container{
+        display: flex;
+        margin-bottom: 4rem;
+    }
+
+    .contact_content {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .contact_input{
+        outline: none;
+        border:none;
+        border-bottom: solid;
+        border-bottom-width: 0.1rem;
+        border-block-color: #2c3e50;
+    }
+
     
 </style>
