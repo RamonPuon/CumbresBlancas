ee6b61cb49a2a1d7b3e437d9550bce59f6f8539d
ramon_puon <ramon.puon@gmail.com>
2022-05-04 09:37:02 -0500

    proyect page

--- src/components/ProyectPage.vue
+++ src/components/ProyectPage.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-       Proyects 
-    </div>
+   
+    <section class="proyect_container">
+        <div class="proyect_left">
+            Monitoreo de Glaciares
+        </div>
+        <div class="proyect_right">
+            <div class="proyect_upperright">
+                Educación
+            </div>
+            <div class="proyect_bottomright">
+                Cumbres Blancas & Limpias
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,5 +22,35 @@ export default {
 </script>
 
 <style>
-    
+    .proyect_container{
+        display: flex;
+        justify-content: first baseline;
+    }
+
+    .proyect_left{
+        width: 50%;
+        height: 500px;
+        background-image: url("../assets/placeholder.jpg");
+        background-size: cover;
+    }
+
+    .proyect_right{
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+    }
+
+    .proyect_upperright{
+        width: 100%;
+        height: 250px;
+        background-image: url("../assets/placeholder.jpg");
+        background-size: cover;
+    }
+
+    .proyect_bottomright{
+        width: 100%;
+        height: 250px;
+        background-image: url("../assets/placeholder.jpg");
+        background-size: cover;
+    }
 </style>
